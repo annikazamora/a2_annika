d1ed9049cee7983ea0e289f020d1ef2055066ed0
annikazamora <azamora@uwaterloo.ca>
2026-03-12 14:34:11 -0400

    added new image file

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ let font; // global variable to store the loaded font (populated in preload())
 let prevScreen = "home";
 let video;
 let playing = false; // track if the intro video is currently playing
-let videoFinished = false; // track if the intro video has finished playing
+let videoFinished = true; // track if the intro video has finished playing
 let ingredientsDone = false; // track if player has collected all ingredients (starts at false, becomes true when they do) --- IGNORE ---
 
 // Ingredient counters (start at 0, increase when player clicks on ingredient in pantry)
